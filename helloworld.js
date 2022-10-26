432d259941bdae58d7eeb9a70a7bcbc81ef78e38
jlyr <joachimlimyouren@gmail.com>
2022-10-26 19:27:08 +0800

    GIT

---
+++ helloworld.js
@@ -0,0 +1,45 @@
+//create count component with vue
+
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     count: 0
+//   },
+//   methods: {
+    
+//     increment: function() {
+//       this.count++;
+//     },
+//   mounted: function() {
+//     this.increment();
+//   }
+
+
+var app= new Vue({
+  el: '#app',
+  data: {
+    count: 0
+  },
+  methods: {
+    increment: function() {
+      this.count++;
+    }
+  },
+  mounted: function() {
+    this.increment();
+  }
+});
+
+
+
+    
+
+
+
+
+
+
+
+// Path: index.html
+
+
