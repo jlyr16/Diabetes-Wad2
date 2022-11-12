6950206cecf7bccd57d3918b89dc8688f7486057
jlyr <joachimlimyouren@gmail.com>
2022-11-12 23:21:15 +0800

    adasdas

--- recipes.js
+++ recipes.js
@@ -1,23 +1,23 @@
-const APP_ID = "788499ba";
-const APP_KEY = "7f4f98dffe8f38aba786c8a08271185f";
+//const APP_ID = "788499ba";
+//const APP_KEY = "7f4f98dffe8f38aba786c8a08271185f";
 
 let searchButton = document.querySelector("#search")
 var queryAPI = document.getElementById('input')
 
+
 //Add an event listener to the button that runs the function sendApiRequest when it is clicked
 searchButton.addEventListener("click", ()=>{
     console.log("button pressed")
-    //e.preventDefault()
-    //searchQuery = e.target.querySelector('input').value
     sendApiRequest()
 })
+
 
 
 //An asynchronous function to fetch data from the API.
 async function sendApiRequest(){
     const recipeURL = 'https://api.edamam.com/search?app_id=788499ba&app_key=7f4f98dffe8f38aba786c8a08271185f&q=' + queryAPI.value
     console.log(recipeURL)
-    const response = await fetch(recipeURL);
+    const response = await fetch(recipeURL)
     console.log(response)
     let data = await response.json()
     console.log(data)
@@ -51,24 +51,3 @@ function useApiData(data){
     })
     document.querySelector("#results").innerHTML = recipeHTML
 }
-
-/*
-<div class="card" style="width: 18rem;">
-    <img src="${data.hits[1].recipe.image}" class="card-img-top" alt="">
-    <div class="card-body">
-        <h5 class="card-title">${data.hits[1].recipe.label}e</h5>
-        <p class="carbs">Carbohydrates: ${data.hits[2].recipe.totalNutrients.CHOCDF.quantity}${data.hits[2].recipe.totalNutrients.CHOCDF.unit}</p>
-        <a href="${data.hits[1].recipe.url}" class="btn btn-primary">View Recipe</a>
-    </div>
-</div>
-
-<div class="card" style="width: 18rem;">
-    <img src="${data.hits[2].recipe.image}" class="card-img-top" alt="">
-    <div class="card-body">
-        <h5 class="card-title">${data.hits[2].recipe.label}</h5> 
-        <p class="carbs">Carbohydrates: ${data.hits[2].recipe.totalNutrients.CHOCDF.quantity}${data.hits[2].recipe.totalNutrients.CHOCDF.unit}</p>
-        <a href="${data.hits[2].recipe.url}" class="btn btn-primary">View Recipe</a>
-    </div>
-</div>
-
-*/
